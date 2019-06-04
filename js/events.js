eafc77710fb0c660de7f7602a3b81b66b1235610
Taiye Salami <taiye41@gmail.com>
2019-06-04 22:20:34 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -9,8 +9,8 @@ function frameIt() {
   })
 }
 function pressIt() {
-  $(document).on('keydown', function() {
-    if(key.which === 71) {
+  $('#typing').on('keydown', function(e) {
+    if(e.which === 71) {
       alert('G was pressed')
 
     }
