8edb9e29921eed8b00cac152b6543f473136458a
Taiye Salami <taiye41@gmail.com>
2019-06-04 22:13:26 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -3,6 +3,11 @@ $('p').on("click", function() {
       alert('Hey!')
   }
 )}
+function frameIt() {
+  $('img').on("load", function() {
+    $('img').addClass('tasty')
+  })
+}
 $(document).ready(function(){
 
 // call functions here
